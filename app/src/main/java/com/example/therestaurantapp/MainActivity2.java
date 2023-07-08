3e51d97d14bbdba5894109da299dfc80787ebe25
coolestcode23 <derrickjackson6881@gmail.com>
2023-07-08 15:47:08 +0000

    Second Screen commit, string naming correctly done

--- app/src/main/java/com/example/therestaurantapp/MainActivity2.java
+++ app/src/main/java/com/example/therestaurantapp/MainActivity2.java
@@ -6,11 +6,14 @@ import android.graphics.Paint;
 import android.os.Bundle;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity2 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity2_main);
+
+        TextView textView = (TextView) findViewById(R.id.orderTextView);
+        textView.setPaintFlags(textView.getPaintFlags()| Paint.UNDERLINE_TEXT_FLAG);
     }
 }
